9d121da820f0a81d85b2e927c8b702400803f1e2
RenzoAnaya <92555271+RenzoAnaya@users.noreply.github.com>
2023-03-08 15:39:55 -0500

    Add files via upload

--- desafioTCL.sql
+++ desafioTCL.sql
@@ -300,3 +300,41 @@ order by cantidad desc;
 call pedidosPendientes();
 
 select * from productosMasComprados;
+
+
+set autocommit=0;
+
+select * from productosOrden;
+
+savepoint inicio;
+
+start transaction;
+
+delete from productosOrden where idOrder in (1,3,5);
+savepoint eliminar;
+
+
+rollback to inicio;
+
+commit
+
+
+start transaction;
+insert into usuario (nombre, apellido, telefono, mail, direccion) values
+("Pete", "Eren", "947855544","rerdfbgsee@gmail.com", "Avenida jose galan 1010"),
+("Joan", "Arpin", "947867444","rerfdsfdee@gmail.com", "Avenida pimpinela 501"),
+("Matias", "Isla", "947876544","rerefddgfde@gmail.com", "Avenida conquistador 101"),
+("Marco", "Monte", "947832444","rerefdfgde@gmail.com", "Avenida arenales 2010") ;
+savepoint userLot04;
+insert into usuario (nombre, apellido, telefono, mail, direccion) values
+("Mauricio", "guines", "944857999","mau@gmail.com", "Calle Salvador 768"),
+("Roberto", "godines", "944857644","robert@gmail.com", "Calle Sargento 101"),
+("Eugenio", "perez", "963544567","eu97@gmail.com", "Calle dali 890"),
+("Joaquin", "salvo", "963547382","joqui@gmail.com", "Calle rodadoresa 201") ;
+savepoint userLot08;
+
+
+rollback to userLot04;
+
+commit;
+
